0b772858c5689403ce8af48d71e50f1033e21f1e
frank <frank@NICIPPE.we7.local>
2014-06-05 17:20:49 +0100

    Added ability to send mail

--- MarketRatingThrottled/src/com/blinkbox/test/Comments/CommentFetcher.java
+++ MarketRatingThrottled/src/com/blinkbox/test/Comments/CommentFetcher.java
@@ -11,7 +11,17 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.lang.Math;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.net.ssl.SSLSession;
+import javax.mail.Message.RecipientType; 
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
@@ -24,12 +34,14 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang3.builder.CompareToBuilder;
+
 import com.gc.android.market.api.MarketSession;
 import com.gc.android.market.api.MarketSession.Callback;
 import com.gc.android.market.api.model.Market.Comment;
 import com.gc.android.market.api.model.Market.CommentsRequest;
 import com.gc.android.market.api.model.Market.CommentsResponse;
 import com.gc.android.market.api.model.Market.ResponseContext;
+//import com.google.protobuf.Message;
 
 public class CommentFetcher {
 	/**
@@ -612,7 +624,6 @@ public class CommentFetcher {
 	}
 	
 	public List<Comment> getBadComments(List<Comment> pComents, int pRating){
-		
 		List<Comment> badComments = new ArrayList<Comment>(); 
 		for(Comment comment : pComents){
 			 if (comment.getRating() <= pRating){
@@ -620,6 +631,33 @@ public class CommentFetcher {
 			 }
 		}
 		return badComments; 		
+	}
+	
+	
+	public void sendMail(final List<Comment> pComments, final String pFrom, String pTo, String pHost, String pSubject, String pMessage){
+		String host = "localhost";
+	      Properties properties = System.getProperties();
+	      properties.setProperty("mail.smtp.host", host);
+	      Session session = Session.getDefaultInstance(properties);
+	      StringBuilder sb = new StringBuilder();
+	      for(Comment comment : pComments){
+	    	   sb.append(comment.getRating());
+	    	   sb.append(" ");
+	    	   sb.append(comment.getText());
+	    	   sb.append("\n");
+		  }
+
+	      try{
+	         MimeMessage message = new MimeMessage(session);
+	         message.setFrom(new InternetAddress(pFrom));
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(pTo));
+	         message.setSubject(pSubject);
+	         message.setText(pMessage);
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
 	}
 	
 	
